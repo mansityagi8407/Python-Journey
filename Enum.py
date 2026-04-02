d049af6a5b0732acca884cbdea57b37c87685e29
Coding <tyagimansi168>
2026-04-02 17:34:20 +0530

    initial commit

---
+++ Enum.py
@@ -0,0 +1,11 @@
+marks = [12, 34, 56, 8, 67, 54, 98, 31]
+for index,  mark in enumerate(marks):
+    print(mark)
+    if(index == 2):
+        print("Awesome!")
+
+
+
+
+
+""" Enumerate func. is a built-in function that allows to loop over a sequence and get the index and value of each element in the sequence at the same time."""
---
+++ IfElse(SH).py
@@ -0,0 +1,10 @@
+a = 3304444
+b = 3303
+print("A") if a > b else print("=")if a == b else print(b)
+print(9) if a>b else ""
+
+c = 8 if a>b else 0
+print(c)
+
+
+""" result = value_if_true if condition else value_if_false"""
