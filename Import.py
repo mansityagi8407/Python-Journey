6ecccef3dfc064b52ae43f8a028b6031926aba2e
mansityagi8407 <tyagimansi168@gmail.com>
2026-04-04 17:40:57 +0530

    new

---
+++ Import.py
@@ -0,0 +1,19 @@
+import math
+# result = math.sqrt(9)
+# print(result)
+
+# import math as m
+# result = m.sqrt(9)
+# print(result)
+
+
+# # from math import pi
+# from math import *
+# result = pi
+# print(result)
+
+# print(dir(math))
+
+from Mansi import*
+welcome()
+print(Mansi)
---
+++ Mansi.py
@@ -0,0 +1,10 @@
+def welcome():
+    print("Hey you are welcome")
+
+Mansi = "A good girl"
+
+""" Using for only one time print """
+print(__name__)
+if __name__ == "__main__":
+ 
+ welcome()
