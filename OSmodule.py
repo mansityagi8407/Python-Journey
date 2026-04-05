11b0656f7fe22c662160e0d79a162777df2a9afd
mansityagi8407 <tyagimansi168@gmail.com>
2026-04-05 16:12:05 +0530

    new

---
+++ OSmodule.py
@@ -0,0 +1,12 @@
+import os
+
+# if(not os.path.exists("data")):
+#  os.mkdir("data")
+
+# for i in range(0, 10):
+#   os.mkdir(f"data/Day{i+1}")
+
+
+# For RENAME
+for i in range(0, 10):
+  os.rename(f"data/Day{i+1}",f"data/Tutorial{i+1}")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-print(c)
+if(not os.path.exists("data")):
+#  os.mkdir("data")
+
+# for i in range(0, 10):
+#   os.mkdir(f"data/Day{i+1}")
