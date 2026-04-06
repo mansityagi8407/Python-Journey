29a661dc4252253c28f8610e7a3170f2c4184ede
mansityagi8407 <tyagimansi168@gmail.com>
2026-04-06 17:57:49 +0530

    new

--- Loc&Glo.py
+++ Loc&Glo.py
@@ -1,9 +1,21 @@
-x = 4
+# x = 4
+# print(x)
+
+# def hello():
+#     x = 5
+#     print(f"The local x is {x}")
+#     print("Hello Mansi")
+# print(f"The global x is {x}")
+# hello()
+# print(f"The global x is {x}")
+
+
+
+x = 6
 print(x)
 
 def hello():
-    x = 5
-    print(x)
-    print("Hello Mansi")
-
+    global x
+    x = 3
 hello()
+print(x)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-if(not os.path.exists("data")):
-#  os.mkdir("data")
-
-# for i in range(0, 10):
-#   os.mkdir(f"data/Day{i+1}")
+global x
