85cf7a6955dee1806cff6868fb957a8d33297b71
mansityagi8407 <tyagimansi168@gmail.com>
2026-04-14 20:21:17 +0530

    @

--- 1.py
+++ 1.py
@@ -1,7 +1,8 @@
 print("Hello, World!")
 a = 12.3
 b = 47
-print(a+b)
+c = 1
+print(a+b+c)
 print("The type of a is",type(a))
-print("The type of b"
-" is",type(b))
+print("The type of b is",type(b))
+print("The type of c is",type(b))
