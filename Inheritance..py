63717bac76af3f535f2e4202280ef3241bf593d6
mansityagi8407 <tyagimansi168@gmail.com>
2026-04-30 19:36:21 +0530

    Inheritance

---
+++ Inheritance..py
@@ -0,0 +1,14 @@
+class Animal :
+    def speak(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog barks: Woof!")
+
+my_dog = Dog()
+my_dog.speak()
+Animal
+my_dog.bark()
+
+
